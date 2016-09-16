43edc491c9dbd8b467bdd8893fb4c9f52c78b71b
brlarsen <brlarsen@wsd.net>
2016-09-16 15:16:37 -0600

    Added error check for "a" input.

---
+++ chap 3/roots2/roots2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double a;
+	cout << "Please enter a: ";		//prompt
+	cin >> a;
+
+	if (a == 0)
+	{
+		cout << "You may not enter '0'\n";
+		exit(0);
+	}
+
+	double b;
+	cout << "Please enter b: ";		//prompt
+	cin >> b;
+
+	double c;
+	cout << "Please enter c: ";		//prompt
+	cin >> c;
+
+	double x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+	cout << "x1 = " << x1 << endl;
+
+	double x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+	cout << "x2 = " << x2 << endl;
+
+
+	return 0;
+}
