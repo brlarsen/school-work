0bef86e49ba51ca638fddea621a5b8dd5e357794
brlarsen <brlarsen@wsd.net>
2016-10-14 12:15:15 -0600

    Added make_sterling function to sterling.cpp.

--- chap 4/chap 4/sterling.cpp
+++ chap 4/chap 4/sterling.cpp
@@ -11,3 +11,21 @@ sterling make_sterling(int p, int s, int pe)
 
 	return temp;
 }
+
+sterling	make_sterling(int pe)
+{
+	sterling	temp;
+
+	temp.pounds = pe / 240;
+	pe % 240;
+	temp.shillings = pe / 12;
+	temp.pence = pe % 12
+
+		return temp;
+}
+
+sterling add(sterling st1, sterling st2)
+{
+	
+
+}
--- chap 4/chap 4/sterling.h
+++ chap 4/chap 4/sterling.h
@@ -7,4 +7,6 @@ struct sterling
 };
 
 sterling make_sterling(int p, int s, int pe);
-sterling make_sterling(int )
+sterling make_sterling(int pe);
+sterling add(sterling st1, sterling st2);
+void print(sterling st);
