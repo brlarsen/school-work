825b07ede42e76e81e535083dabe26c176e5de10
brlarsen <brlarsen@wsd.net>
2016-10-25 12:53:45 -0600

    Added if statement for scores = 100 to display in correct bin.

--- lab 5/lab 5/stats.cpp
+++ lab 5/lab 5/stats.cpp
@@ -2,7 +2,7 @@
 #include <iomanip>
 using namespace std;
 
-void histogram(int *scores, int count);
+void histogram(int *scores, int count, int(&histogram)[10]);
 double deviation(int *scores, int count);
 double mean(int *scores, int count);
 
@@ -10,12 +10,13 @@ int main()
 {
 	int scores[100];
 	int score;
+	int calculatedHistogram[10] = { 0,0,0,0,0,0,0,0,0,0 };
 	int count = 0;
 
 	cout << "Enter a score (-1 to stop): ";
 
 	cin >> score;
-	while (score <= -1)
+	while (score > -1)
 
 	{
 		scores[count++] = score;
@@ -23,17 +24,38 @@ int main()
 		cin >> score;
 	}
 
+	histogram(scores, count, calculatedHistogram);
+
+	for (int index = 9; index >= 0; index--)
+	{
+		cout << index << "| ";
 
 
-
+		for (int i = 0; i < calculatedHistogram[index]; i++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+	cout << "SD: " << deviation(scores, count) << endl;
+	int tempDev;
+	cin >> tempDev;
 	return 0;
 }
 
-void histogram(int *scores, int count)
+void histogram(int *scores, int count, int (&histogram)[10])
 {
-	;
+	for (int i = 0; i < count; i++)
+	{
 
+		int temp = static_cast<int>(scores[i] / 10);
+		if (temp >= 10)
+			temp = 9;
+		histogram[temp]++;
+	}
+	
 
+	//return histogram;
 }
 
 double deviation(int *scores, int count)
